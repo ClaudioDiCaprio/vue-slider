18a60829b84419036b69fb6eafc3e9e75e0edf56
Claudio <claudiodicaprio89@gmail.com>
2021-11-26 16:30:59 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const app = new Vue({
+    el:'#root',
+    data: {
+        images:[
+            " img/01.jpg",
+            " img/02.jpg",
+            " img/03.jpg",
+            " img/04.jpg",
+            " img/05.jpg",
+        ],
+        currentImg:0
+    },
+    methods:{
+        nextImage:function(){
+            if(this.currentImg== this.images.length - 1 ){
+                this.currentImg =0;
+            }else{
+                this.currentImg++;
+            }
+            
+        },
+        prevImg:function(){
+            if(this.currentImg == 0){
+                this.currentImg = this.images.length-1;
+            }else{
+                this.currentImg--;
+            }    
+        }
+
+    }
+});
